01d5bc1998b3fdc092a64d5b6a38508d7102e5ef
Rajiv <lucky81205@gmail.com>
2025-03-30 14:31:09 +0530

    Update title in index.html, add custom scrollbar styles in index.css, and enhance Hero component with video background and layout adjustments.

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,10 +3,13 @@ import Spline from '@splinetool/react-spline';
 
 const Hero = ({ onLoad }) => {
   return (
-    <div>
+    <div className='h-screen w-screen relative'>
+        <video src="/bg.mp4" autoPlay muted loop className='w-full h-full object-cover' />
+        <div className='absolute inset-0 bg-black/80' />
       <Spline 
         scene="https://prod.spline.design/0CjTKi3v6AEI-cUl/scene.splinecode"
         onLoad={onLoad}
+        className='absolute inset-0 z-10'
       />
     </div>
   )
