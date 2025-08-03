9657343069ccd95befe4357d5514ca1acc796590
Rajiv Dubey <161409149+RAJIV81205@users.noreply.github.com>
2025-08-03 14:19:20 +0530

    Enhance Hero component with improved navigation styles, adding hover effects and transitions for a more interactive user experience.

--- components/Landing/Hero.tsx
+++ components/Landing/Hero.tsx
@@ -12,14 +12,26 @@ const Hero = () => {
       </div>
       <nav className='flex items-center w-1/2'>
         <ul className='flex items-center w-full justify-evenly text-xl text-primary font-kode-mono'>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="#about">About</Link></li>
-          <li><Link href="#features">Features</Link></li>
-          <li><Link href="#contact">Contact</Link></li>
+          <li className='group relative hover:text-purple-900 transition-all duration-300 hover:font-semibold'>
+            <Link href="/">Home</Link>
+            <span className='absolute bottom-0 left-0 w-0 h-0.5 bg-purple-900 transition-all duration-300 group-hover:w-full'></span>
+          </li>
+          <li className='group relative hover:text-purple-900 transition-all duration-300 hover:font-semibold'>
+            <Link href="#about">About</Link>
+            <span className='absolute bottom-0 left-0 w-0 h-0.5 bg-purple-900 transition-all duration-300 group-hover:w-full'></span>
+          </li>
+          <li className='group relative hover:text-purple-900 transition-all duration-300 hover:font-semibold'>
+            <Link href="#features">Features</Link>
+            <span className='absolute bottom-0 left-0 w-0 h-0.5 bg-purple-900 transition-all duration-300 group-hover:w-full'></span>
+          </li>
+          <li className='group relative hover:text-purple-900 transition-all duration-300 hover:font-semibold '>
+            <Link href="#contact">Contact</Link>
+            <span className='absolute bottom-0 left-0 w-0 h-0.5 bg-purple-900 transition-all duration-300 group-hover:w-full'></span>
+          </li>
         </ul>
       </nav>
       <div className='flex items-center justify-center text-secondary'>
-        <button className='rounded-full bg-primary  px-4 py-2 flex items-center gap-2'>LOGIN <MoveUpRight className='w-4 h-4' /></button>
+        <button className='rounded-full bg-primary px-4 py-2 flex items-center gap-2'>LOGIN <MoveUpRight className='w-4 h-4' /></button>
       </div>
 
       </header>
