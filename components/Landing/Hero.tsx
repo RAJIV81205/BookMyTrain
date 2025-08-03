9ec02d98152054090fab361a7d4e734e8eca7077
Rajiv Dubey <161409149+RAJIV81205@users.noreply.github.com>
2025-08-03 14:29:43 +0530

    Update Hero component to include a title and remove the logo image, enhancing the overall layout and user navigation experience.

--- components/Landing/Hero.tsx
+++ components/Landing/Hero.tsx
@@ -1,14 +1,19 @@
+'use client'
+
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { MoveUpRight } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 const Hero = () => {
+  const router = useRouter();
   return (
     <div className='h-[90vh] w-full bg-[url(/train-main.jpg)] bg-cover bg-center bg-no-repeat text-black '>
       <header className='w-full h-20 bg-transparent flex items-center justify-between px-10 mt-4 font-kode-mono'>
         <div className='flex items-center justify-center'>
-          <Image src="/logo.png" alt="logo" width={100} height={100} />
+          {/* <Image src="/logo.png" alt="BookMyTrain" width={100} height={100} className='cursor-pointer' onClick={() => router.push('/')} /> */}
+          <h1 className='text-2xl font-kode-mono font-bold text-primary'>BookMyTrain</h1>
         </div>
         <nav className='flex items-center w-1/2'>
           <ul className='flex items-center w-full justify-evenly text-xl text-primary font-kode-mono'>
