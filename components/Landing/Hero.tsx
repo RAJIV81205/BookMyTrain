8a7a17cf6b4c9d395c6f96d1f9d69b706c488cea
Rajiv Dubey <161409149+RAJIV81205@users.noreply.github.com>
2025-08-03 14:02:55 +0530

    Add Kode Mono font and update styles in globals.css; enhance Hero component with navigation and login button

--- components/Landing/Hero.tsx
+++ components/Landing/Hero.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { MoveUpRight } from 'lucide-react';
 
 const Hero = () => {
   return (
-    <div className='h-[90vh] w-full bg-[url(/train-main.jpg)] bg-cover bg-center bg-no-repeat'>
-        <header>
-          
-        </header>
+    <div className='h-[90vh] w-full bg-[url(/train-main.jpg)] bg-cover bg-center bg-no-repeat text-black '>
+      <header className='w-full h-20 bg-transparent flex items-center justify-between px-10 mt-4 font-kode-mono'>
+      <div className='flex items-center justify-center'>
+        <Image src="/logo.png" alt="logo" width={100} height={100} />
+      </div>
+      <nav className='flex items-center w-1/2'>
+        <ul className='flex items-center w-full justify-evenly text-xl text-primary font-kode-mono'>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="#about">About</Link></li>
+          <li><Link href="#features">Features</Link></li>
+          <li><Link href="#contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <div className='flex items-center justify-center text-secondary'>
+        <button className='rounded-full bg-primary  px-4 py-2 flex items-center gap-2'>LOGIN <MoveUpRight className='w-4 h-4' /></button>
+      </div>
+
+      </header>
+       
     </div>
   )
 }
