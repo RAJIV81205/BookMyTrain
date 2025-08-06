81ddf071d72ff6f32059b86086aea1515dcf2a16
Rajiv Dubey <161409149+RAJIV81205@users.noreply.github.com>
2025-08-06 09:58:29 +0530

    Add Header, Hero, and Footer components; establish layout in page.tsx and define global styles in globals.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import Landing from '@/components/Landing/Landing'
+import Header from '@/components/Landing/Header'
+import Hero from '@/components/Landing/Hero'
+import Footer from '@/components/Landing/Footer'
 
 const page = () => {
   return (
-    <Landing />
+    <main className="min-h-screen flex flex-col">
+ 
+      <Hero />
+
+    </main>
   )
 }
 
